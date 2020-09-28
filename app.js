d038888f4e1827a1e0f607c3a2a84dbcca5f4a85
aspau <27313480+aspau@users.noreply.github.com>
2020-09-28 14:52:43 -0400

    Added basic server framework

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const compress = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(helmet());
+
+app.use(cors({methods: ['GET,POST']}));
+
+
+app.use(compress());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('webhook listening on port ' + port);
+
